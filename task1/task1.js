edb12a55ee14263b28d88ae49b836a2261193864
dmytro-revak <dmytro27@gmail.com>
2016-12-12 14:10:02 +0200

    add default param. to task1 files

--- task1/task1.js
+++ task1/task1.js
@@ -1,7 +1,7 @@
 (function showTheMessage() {
-	var name = prompt('Please enter your name');
-	var event = prompt('Which event do you have today ?');
-	var time = prompt('What time does this event start ?');
-	var place = prompt('Where does the event start ?');
+	var name = prompt('Please enter your name', 'Walter White');
+	var event = prompt('Which event do you have today ?', 'cooking event');
+	var time = prompt('What time does this event start ?', '12 a.m.');
+	var place = prompt('Where does the event start ?', 'meth lab');
 	console.log(name + " has a " + event + " today at " + time + " in " + place);
 })();
