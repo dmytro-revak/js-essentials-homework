378ead2bf6b9bf5777b511aca0a697a0b38feabb
dmytro-revak <dmytro27@gmail.com>
2016-12-12 02:43:51 +0200

    fixed task 7

--- task7/task7.js
+++ task7/task7.js
@@ -2,6 +2,7 @@
   var numberOfGame = 1;
   var sizeOfPrize = 1;
   var finalPrize = 0;
+  var sizeOfGap = 5;
   askYouWantToPlay();
 
   function askYouWantToPlay() {
@@ -15,41 +16,41 @@
   }
 
   function playTheGame(numberOfGame, sizeOfPrize) {
-    var gap = numberOfGame * 5 + 1;
+    var gap = sizeOfGap + 1;
     var ramdomNumber = Math.floor((Math.random() * gap));
-    var haveYouguessedNumber = false;
+    var haveYouGuessedNumber = false;
     for (var i = 1; i < 4; i++) {
-      var usersNumber = parseInt(prompt('This is your ' + i + ' attempt. Please. enter your number'));
+      var usersNumber = parseInt(prompt('This is your ' + i + ' attempt. Please. enter your number from 0 to ' + sizeOfGap));
       if (usersNumber === ramdomNumber && i === 1) {
-        console.log('Your win - 10 $');
+        console.log('Your win - ' + 10 * sizeOfPrize + ' $');
         finalPrize += 10 * sizeOfPrize;
-        haveYouguessedNumber = true;
+        haveYouGuessedNumber = true;
         break;
       } else if (usersNumber === ramdomNumber && i === 2) {
-        console.log('Your win - 5 $');
+        console.log('Your win - ' + 5 * sizeOfPrize + ' $');
         finalPrize += 5 * sizeOfPrize;
-        haveYouguessedNumber = true;
+        haveYouGuessedNumber = true;
         break;
       } else if (usersNumber === ramdomNumber && i === 3) {
-        console.log('Your win - 2 $');
+        console.log('Your win - ' + 2 * sizeOfPrize + ' $');
         finalPrize += 2 * sizeOfPrize;
-        haveYouguessedNumber = true;
+        haveYouGuessedNumber = true;
         break;
       } else if (usersNumber !== ramdomNumber && i === 3) {
         console.log('Your win - 0$');
       }
     }
 
-debugger;
     continueTheGame();
 
     function continueTheGame() {
       var doYuoWantToPlayAgain = confirm('Do you want to play the game one more time ?');
-      if (doYuoWantToPlayAgain == true && haveYouguessedNumber == true) {
-        numberOfGame++;
+      if (doYuoWantToPlayAgain == true && haveYouGuessedNumber == true) {
         sizeOfPrize = Math.pow(3,numberOfGame);
+        numberOfGame++;
+        sizeOfGap *= 2;
         playTheGame(numberOfGame, sizeOfPrize);
-      } else if (doYuoWantToPlayAgain == true && haveYouguessedNumber == false) {
+      } else if (doYuoWantToPlayAgain == true && haveYouGuessedNumber == false) {
         playTheGame(numberOfGame, sizeOfPrize);
       } else {
         console.log('Thanks for the game! Your win is ' + finalPrize + ' $');
