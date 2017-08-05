7220a6f8285ab8a58a1a98d6710e2b501f78fd09
dmytrorevak <dmytro.revak@gmail.com>
2017-08-05 19:32:34 +0300

    add test comment

--- sum.js
+++ sum.js
@@ -29,12 +29,14 @@ function randomNumberFromMinWithMax(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min) + min);
 }
 
-// sumToNumbers();
-// console.log( getDecimal(12.345) );
-// console.log( getDecimal(1.2) );
-// console.log( getDecimal(-1.2) );
-// console.log( fibBinet(3) );
-// console.log( fibBinet(77) );
-// console.log(randomNumberFromZeroToMax(5));
-// console.log(randomNumberFromMinToMax(4, 10));
-// console.log(randomNumberFromMinWithMax(1, 3));
+// Test for functions
+
+sumToNumbers();
+console.log( getDecimal(12.345) );
+console.log( getDecimal(1.2) );
+console.log( getDecimal(-1.2) );
+console.log( fibBinet(3) );
+console.log( fibBinet(77) );
+console.log(randomNumberFromZeroToMax(5));
+console.log(randomNumberFromMinToMax(4, 10));
+console.log(randomNumberFromMinWithMax(1, 3));
