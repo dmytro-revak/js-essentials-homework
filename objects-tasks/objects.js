a74e884333ce7bf4cbb17191055293b4bded4a9a
dmytrorevak <dmytro.revak@gmail.com>
2017-08-09 12:16:48 +0300

    create the function that counts total employees salary in the certain object

--- objects-tasks/objects.js
+++ objects-tasks/objects.js
@@ -1,8 +1,38 @@
+/**
+The function checks if the accepted object is empty.
+ */
+
+
 function isEmpty(object) {
     return !Boolean(Object.keys(object).length);
 }
 
+/*
 var schedule = {};
 console.log(isEmpty(schedule));
 schedule["8:30"] = "Wake up";
-console.log(isEmpty(schedule));
+console.log(isEmpty(schedule)); 
+*/
+
+
+/**
+
+ */
+
+
+function countSalaries(salariesObject) {
+    var totalSalary = 0;
+
+    for (var employee in salariesObject) {
+        totalSalary += salariesObject[employee];
+    }
+
+    return totalSalary;
+}
+
+var salaries = {
+    "Oleg": 100,
+    "Ostap": 300,
+    "Julia": 250
+};
+console.log(countSalaries(salaries));
