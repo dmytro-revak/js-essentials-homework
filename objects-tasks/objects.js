3110eb56cea9c97759768127e0aa6b67ebc5ce26
dmytrorevak <dmytro.revak@gmail.com>
2017-08-09 13:44:08 +0300

    create the function that provides deeply recursive copying of accepted object.

--- objects-tasks/objects.js
+++ objects-tasks/objects.js
@@ -63,10 +63,12 @@ function getHighestPaidEmployee(salariesObject) {
 // console.log(getHighestPaidEmployee(salaries));
 
 
-
+/**
+The function multiplies all numeric properties in the accepted object.
+ */
 
 function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function multiplyNumeric(object) {
@@ -83,6 +85,54 @@ var menu = {
     title: "My menu"
 };
 
-console.log(menu);
-multiplyNumeric(menu);
-console.log(menu);
+// console.log(menu);
+// multiplyNumeric(menu);
+// console.log(menu);
+
+
+/**
+The function provides deeply recursive copying of accepted object.
+ */
+
+
+function recursiveObjectCopy(object) {
+    var objectCopy = (Array.isArray(object)) ? [] : {};
+
+    for (var key in object) {
+        copyObjectProperty(objectCopy, object, key);
+    }
+
+    return objectCopy;
+
+    function copyObjectProperty(objectCopy, originalObject, objectProperty) {
+        if (originalObject[objectProperty] instanceof Object ||
+            originalObject[objectProperty] instanceof Array) {
+            objectCopy[objectProperty] = recursiveObjectCopy(originalObject[objectProperty]);
+        } else {
+            objectCopy[objectProperty] = originalObject[objectProperty];
+        }
+    }
+}
+
+var testObject = {
+    prop: 'prop',
+    prop2: 'prop2',
+    num: 1,
+    arr: ['some', 'arr'],
+    num2: 2,
+    obj: {
+        some: 'object'
+    },
+    arr2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    obj2: {
+        test: {
+            prop: 'nested ong',
+            nestedArr: ['nested', 'arr']
+        },
+        test2: 'test2',
+        test3: 'test3'
+    }
+};
+
+console.log(testObject);
+console.log(recursiveObjectCopy(testObject));
