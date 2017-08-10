b58c6c1c251abb9967d62e75570f47b05fcc1b52
dmytrorevak <dmytro.revak@gmail.com>
2017-08-10 12:44:34 +0300

    create function that  simulates the arrays push function

--- arrays-tasks/arrays.js
+++ arrays-tasks/arrays.js
@@ -7,8 +7,24 @@ function getLastElement(array) {
     return array[array.length - 1];
 }
 
-var goods = ['meat', 'milk', 'apple', 'tomato'];
-console.log(getLastElement(goods));
-goods.push('potato');
-console.log(getLastElement(goods));
+// var goods = ['meat', 'milk', 'apple', 'tomato'];
+// console.log(getLastElement(goods));
+// goods.push('potato');
+// console.log(getLastElement(goods));
+
+
+/**
+ The function simulates the arrays push function.
+ */
+
+
+function arrayPush(array, element) {
+    return array[array.length] = element;
+}
+
+// var users = ['tom', 'mike'];
+// console.log(users);
+// arrayPush(users, 'gary');
+// console.log(users);
+
 
