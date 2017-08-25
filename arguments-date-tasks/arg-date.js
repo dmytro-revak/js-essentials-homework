bfcf610a4ce58d50c32e390bbe4988a2e0af6a14
dmytrorevak <dmytro.revak@gmail.com>
2017-08-25 14:50:44 +0300

    finished with "arguments" tasks

--- arguments-date-tasks/arg-date.js
+++ arguments-date-tasks/arg-date.js
@@ -1,1 +1,31 @@
-alert(1);
+/**
+ The function verifies has user entered the first argument.
+ */
+
+
+function checkFirstArgument() {
+    var isArgument = Boolean(arguments.length),
+        result = (isArgument) ? 'There is first argument' : 'There is not first argument';
+
+    return result;
+}
+
+// console.log(checkFirstArgument());
+// console.log(checkFirstArgument(1));
+
+
+function allArgumentsSum() {
+    var result = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+
+    return result;
+}
+
+console.log(allArgumentsSum());
+console.log(allArgumentsSum(1));
+console.log(allArgumentsSum(1, 2));
+console.log(allArgumentsSum(1, 2, 3));
+console.log(allArgumentsSum(1, 2, 3, 4));
