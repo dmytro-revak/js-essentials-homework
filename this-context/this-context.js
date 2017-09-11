9d634918c09e8d295fef0c4565b766ce9cda054c
dmytrorevak <dmytro.revak@gmail.com>
2017-09-11 13:14:30 +0300

    create calculator object using this context

---
+++ this-context/this-context.js
@@ -0,0 +1,21 @@
+/**
+ Calculator object using this context.
+ */
+
+
+var calculator = {
+    read: function() {
+        this.firstNumber = parseInt(prompt('Enter the first number: '));
+        this.secondNumber = parseInt(prompt('Enter the second number: '));
+    },
+    sum: function() {
+        return this.firstNumber + this.secondNumber;
+    },
+    mul: function() {
+        return this.firstNumber * this.secondNumber;
+    }
+};
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.mul());
